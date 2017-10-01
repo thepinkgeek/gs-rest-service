c482f1c7219cdb658ded0e361e2a16f64bc98126
Angelee Pinzon <angelee.pinzon@gmail.com>
2017-10-01 19:53:15 +0800

    Add main springboot class

---
+++ initial/src/main/java/common/Application.java
@@ -0,0 +1,13 @@
+package common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"controller", "model"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
